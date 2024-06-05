6e25d243e02cdfca9e91cd57670b3248c4d6317f
kapoorkartik <kartikkapoor110@gmail.com>
2024-06-05 11:03:30 +0530

    update title and logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Payment from './components/Payment';
-import { Thanks } from './components/Thanks';
-import MyForm from './components/Form';
 import Homepage from './components/HomePage';
 const App = () => {
   return (
